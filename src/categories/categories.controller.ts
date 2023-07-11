01e86b44071f9ba7aa964006918dc7ac94dead3c
steevened <alvaradosteven315@gmail.com>
2023-07-10 23:09:13 -0500

    secure update and delete category routes with authGuards and RolesGuard

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -14,13 +14,14 @@ import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Roles } from 'src/roles.decorator';
 import { Role } from 'src/role.enum';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
-  @UseGuards()
+  @UseGuards(AuthGuard)
   @Roles(Role.ADMIN)
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoriesService.create(createCategoryDto);
@@ -36,6 +37,8 @@ export class CategoriesController {
     return this.categoriesService.findOneBySlug(slug);
   }
 
+  @UseGuards(AuthGuard)
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(
     @Param('id', ParseUUIDPipe) id: string,
@@ -44,6 +47,8 @@ export class CategoriesController {
     return this.categoriesService.update(id, updateCategoryDto);
   }
 
+  @UseGuards(AuthGuard)
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.categoriesService.remove(id);
