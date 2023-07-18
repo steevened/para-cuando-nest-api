02154a2b30e762bd0cbb1deb685946f25517d2f1
steevened <alvaradosteven315@gmail.com>
2023-07-17 22:22:45 -0500

    create pet dto

--- src/pets/dto/create-pet.dto.ts
+++ src/pets/dto/create-pet.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePetDto {}
+import { IsString } from 'class-validator';
+
+export class CreatePetDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  kind: string;
+
+  @IsString()
+  breed: string;
+
+  @IsString()
+  city: string;
+
+  @IsString({ each: true })
+  image_url: string[];
+}
